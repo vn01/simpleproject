0e3a082ea8dcb1590353b2a5117d700d7e9bf58f
vn01 <vnraj01@gmail.com>
2021-09-15 21:52:32 +0000

    docker new changes

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello {
     public static void main(String[] args) {
-      System.out.println("This is a java app created using Docker");
+      System.out.println("This is a java app created using Docker-automation");
     }
   }
